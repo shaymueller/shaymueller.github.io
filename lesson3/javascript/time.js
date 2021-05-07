141bd6677bf3ef74c2f1d469132eed0a1f0aeaaa
shaymueller <Enter your email address>
2021-05-07 00:27:27 -0600

    update

---
+++ lesson3/javascript/time.js
@@ -0,0 +1,6 @@
+//current year
+let time = new Date();
+document.querySelector('#currentYear').textContent = time.getFullYear();
+
+// for latest edit on home page
+document.querySelector('#todayDate').textContent = new Date;
