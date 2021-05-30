308e227dc3cc98b8f5906de287948dae11781d29
shaymueller <Enter your email address>
2021-05-29 23:13:08 -0600

    update

--- lesson6/javascript/windchill.js
+++ lesson6/javascript/windchill.js
@@ -1,20 +1,12 @@
 // wind chill
-function tempSpeed() {
-    document.querySelector('#speed').innerHTML = 21;   
-    let speed = document.getElementById('speed').innerHTML;  
+let s = document.getElementById("speed").innerHTML;
+let t = document.getElementById("tempF").innerHTML;
+var result;
 
-    document.querySelector('#tempF').innerHTML = 47;     
-    let tempF = document.getElementById('tempF').innerHTML;
-
-
-    let currentChill = windChill(tempF, speed);
-    document.querySelector('#windChill').textContent = currentChill;
+if(t <= 50 && s >= 3){
+    wind = Math.round(35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t * (s ** 0.16)));
+    document.getElementById("windChill").innerHTML = wind;
 }
-function windChill(t, s) {
-     if (t <= 50 && s > 3) {
-        let f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
-        return f;
-    } else {
-        return "N/A";
-    }
+else{
+    document.getElementById("windChill").innerHTML = "N/A";
 }
